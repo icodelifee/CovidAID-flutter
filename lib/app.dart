63813bada5104dd7074d79d0adbca3cf4a342d85
icodelifee <mithunsmenon@gmail.com>
2021-05-01 22:08:19 +0530

    feat: implement splashscreen

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lifecoronasafe/ui/Homepage/Homepage.dart';
 // import 'package:lifecoronasafe/ui/Homepage/Homepage.dart';
-import 'package:lifecoronasafe/ui/Splashscreen/Splashscreen.dart';
+import 'package:lifecoronasafe/ui/Splashscreen/Splashscreen.dart' as splash;
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:your_splash/your_splash.dart';
 
 class App extends StatelessWidget {
-  // This widget is the root of your application.
+  final List<GetPage> _pages = [
+    GetPage(name: '/', page: () => HomePage()),
+    GetPage(name: '/splash', page: () => const splash.SplashScreen()),
+  ];
+  final ThemeData themeData =
+      ThemeData(textTheme: GoogleFonts.poppinsTextTheme());
+  final Transition transition = Transition.cupertino;
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        defaultTransition: Transition.cupertino,
-        title: 'Life By Corona Safe',
-        theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
-        home: SplashScreen());
+      debugShowCheckedModeBanner: false,
+      defaultTransition: transition,
+      title: 'Life By Corona Safe',
+      theme: themeData,
+      getPages: _pages,
+      home: SplashScreen.timed(
+        route: MaterialPageRoute(builder: (_) => HomePage()),
+        seconds: 3,
+        body: HomePage(),
+      ),
+    );
   }
 }
