673b03f849de40d7512084fc15f6a9bb1a9eb146
icodelifee <mithunsmenon@gmail.com>
2021-05-01 21:15:46 +0530

    feat: use GoogleFonts Poppins as default font

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:lifecoronasafe/ui/Homepage/Homepage.dart';
+import 'package:google_fonts/google_fonts.dart';
+// import 'package:lifecoronasafe/ui/Homepage/Homepage.dart';
+import 'package:lifecoronasafe/ui/Splashscreen/Splashscreen.dart';
 
 class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      defaultTransition: Transition.cupertino,
-      title: 'Life By Corona Safe',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        defaultTransition: Transition.cupertino,
+        title: 'Life By Corona Safe',
+        theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
+        home: SplashScreen());
   }
 }
