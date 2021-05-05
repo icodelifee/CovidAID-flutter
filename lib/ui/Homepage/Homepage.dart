c019ed2a8b59f01d7a2a1158ce3ddac547b40a8d
icodelifee <mithunsmenon@gmail.com>
2021-05-06 00:21:14 +0530

    ui(home): implement textfields in homepage

--- lib/ui/Homepage/Homepage.dart
+++ lib/ui/Homepage/Homepage.dart
@@ -1,16 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lifecoronasafe/theme/app_theme.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.black),
         backgroundColor: Colors.transparent,
         elevation: 0,
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.notifications_active_outlined),
+              onPressed: () {})
+        ],
       ),
+      drawer: const Drawer(),
       body: Container(
-        padding: EdgeInsets.all(20),
-        child: ListView(),
+        padding: const EdgeInsets.all(20),
+        child: Stack(
+          children: [
+            HomeLifeLogo(),
+            Align(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  DropdownButtonFormField(
+                    isExpanded: true,
+                    hint: Text('Select State'),
+                    decoration: AppTheme.inputDecoration(''),
+                    items: [],
+                  ),
+                  Gap(15),
+                  DropdownButtonFormField(
+                    isExpanded: true,
+                    hint: Text('Select District'),
+                    decoration: AppTheme.inputDecoration(''),
+                    items: [],
+                  ),
+                  Gap(15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: Get.width / 2.1,
+                        child: DropdownButtonFormField(
+                          isExpanded: true,
+                          hint: Text('Select Resource'),
+                          decoration: AppTheme.inputDecoration(''),
+                          items: [],
+                        ),
+                      ),
+                      SizedBox(
+                          width: Get.width / 2.8,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Verified',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Checkbox(value: false, onChanged: (val) {})
+                            ],
+                          ))
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomeLifeLogo extends StatelessWidget {
+  const HomeLifeLogo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/images/life-filled.png',
+            height: Get.width / 3,
+          ),
+          Text(
+            'Life',
+            style:
+                GoogleFonts.poppins(fontWeight: FontWeight.w800, fontSize: 50),
+          ),
+        ],
       ),
     );
   }
