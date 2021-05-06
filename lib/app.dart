c50823149d445cc870c5af5fbd09aa4303720224
icodelifee <mithunsmenon@gmail.com>
2021-05-06 22:29:27 +0530

    fix: disable splashscreen

--- lib/app.dart
+++ lib/app.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lifecoronasafe/firebase/firebase_authentication.dart';
 import 'package:lifecoronasafe/ui/Splashscreen/splash_screen.dart' as splash;
-import 'package:lifecoronasafe/ui/pages/home_page.dart';
+import 'package:lifecoronasafe/ui/homepage/home_page.dart';
 import 'package:your_splash/your_splash.dart';
 
 class App extends StatelessWidget {
@@ -19,16 +19,11 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     FirebaseAuthentication.signInAnonymously();
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      defaultTransition: transition,
-      title: 'Life By Corona Safe',
-      theme: themeData,
-      getPages: _pages,
-      home: SplashScreen.timed(
-        route: MaterialPageRoute(builder: (_) => HomePage()),
-        seconds: 4,
-        body: splash.SplashScreen(),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        defaultTransition: transition,
+        title: 'Life By Corona Safe',
+        theme: themeData,
+        getPages: _pages,
+        home: HomePage());
   }
 }
