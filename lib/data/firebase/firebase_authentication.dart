819b8dbb3167f8d44cbf21b28f6e785ae598a979
icodelifee <mithunsmenon@gmail.com>
2021-05-07 01:03:56 +0530

    feat: create constants class and move VAPID key to it

--- lib/data/firebase/firebase_authentication.dart
+++ lib/data/firebase/firebase_authentication.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lifecoronasafe/constants.dart';
 import 'package:lifecoronasafe/data/firebase/firestore_db.dart';
 
 class FirebaseAuthentication {
@@ -11,14 +12,12 @@ class FirebaseAuthentication {
 
   static late UserCredential userCredential;
   static late User user;
-  static const String VAPID_KEY =
-      'BDemkz-pmd4urZdI_pej_b-PUra7MXcY7GfgRaurNgr3956MrwgCSmlBgKCkSAIjAGAo5b5e_TOefQIS20QDI2o';
 
   static Future<void> signInAnonymously() async {
     try {
       userCredential = await firebaseAuthInstance.signInAnonymously();
-      final String token =
-          (await FirebaseMessaging.instance.getToken(vapidKey: VAPID_KEY))!;
+      final String token = (await FirebaseMessaging.instance
+          .getToken(vapidKey: Constants.VAPID_KEY))!;
       FireStoreDb.addUser(firebaseAuthInstance.currentUser!.uid, token);
     } catch (e) {
       debugPrint(e.toString());
