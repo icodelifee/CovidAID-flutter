e9f693c6de54ffcdd8eebd9fa3c42fbc18d8cf98
icodelifee <mithunsmenon@gmail.com>
2021-05-08 22:38:27 +0530

    feat: add method to fetch resources

--- lib/data/firebase/firestore_db.dart
+++ lib/data/firebase/firestore_db.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
+import 'package:lifecoronasafe/models/resource.dart';
 
 class FireStoreDb {
   static FirebaseFirestore get fireStoreInstance {
@@ -26,6 +28,20 @@ class FireStoreDb {
       return 'Update Success';
     } catch (e) {
       return e.toString();
+    }
+  }
+
+  static Future<List<Resource>> getAllResources() async {
+    try {
+      final snapshot =
+          await fireStoreInstance.collection('data').doc('resources').get();
+      final rMap = snapshot.data()!['data'] as List;
+      return rMap
+          .map((e) => Resource.fromJson(e as Map<String, dynamic>))
+          .toList();
+    } catch (e) {
+      debugPrint(e.toString());
+      throw FirebaseException(message: e.toString(), plugin: '');
     }
   }
 }
