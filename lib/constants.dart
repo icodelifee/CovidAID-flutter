c2707025726e948e737868c847530c16f21157e0
icodelifee <mithunsmenon@gmail.com>
2021-05-10 23:11:25 +0530

    feat: add lifepipeline api url in constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,4 +3,6 @@ class Constants {
   static const String VAPID_KEY =
       'BDemkz-pmd4urZdI_pej_b-PUra7MXcY7GfgRaurNgr3956MrwgCSmlBgKCkSAIjAGAo5b5e_TOefQIS20QDI2o';
   static const lifeAPI = 'https://life-api.coronasafe.network/data/';
+  static const lifePipelineAPI =
+      'https://life-pipeline.coronasafe.network/api/resources?';
 }
