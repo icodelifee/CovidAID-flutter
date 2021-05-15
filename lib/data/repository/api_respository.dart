eef82c394cc79e10cbb1c6de016e66cf99e1bdfb
icodelifee <mithunsmenon@gmail.com>
2021-05-15 20:14:09 +0530

    refractor: remove unused api repository code

--- lib/data/repository/api_respository.dart
+++ lib/data/repository/api_respository.dart
@@ -2,7 +2,6 @@ import 'package:lifecoronasafe/data/models/covid_resource_model.dart';
 
 abstract class APIRepository {
   Future<List<String>> getAllStates();
-  Future<List<String>> getSearchResults(String query);
   Future<CovidResources?> fetchResources(
       String state, String district, String resource);
   Future<Map<String, dynamic>> getMappedPlaces();
